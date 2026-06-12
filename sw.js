83f351d3130fdf9a283f12ab18428e40bb8091bf
atsun443629-max <atsun443629@gmail.com>
2026-06-13 07:31:10 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// 内外ワイヤー Service Worker
+// アプリ本体（シェル）はキャッシュから即時表示し、ニュースは常にネットワークから取得する
+const CACHE = "naigai-wire-v1";
+const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  // 自サイトのファイルはキャッシュ優先（即時起動）、ニュース等の外部通信はネットワーク直行
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then((hit) => hit || fetch(e.request))
+    );
+  }
+});
